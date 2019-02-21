3bfbfd9eb7522b81050c485e35a1549f604ce80c
codinronan <patrick@engineerapart.com>
2019-02-21 00:49:13 -0600

    Add initial hook rules example

--- src/rules-of-hooks.js
+++ src/rules-of-hooks.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // https://reactjs.org/docs/hooks-rules.html
+// yarn add --dev eslint-plugin-react-hooks
+// "eslintConfig": {
+//   "extends": [
+//     "react-app"
+//   ],
+//   "plugins": [
+//     "react-hooks"
+//   ],
+//   "rules": {
+//     "react-hooks/rules-of-hooks": "error"
+//   }
+// },
 
-export default function Rules() {
-  return <div className="rules-of-hooks"></div>;
+function TopLevel({ fail }) {
+  const [state, setState] = useState(fail); // eslint-disable-line
+
+  if (state) {
+    // This will force a re-render, causing state to change, breaking the condition -
+    // thus changing the number of hooks that get rendered.
+    useEffect(() => setState(!state));
+  }
+
+  return <div><p>TopLevel Failed: {JSON.stringify(state)}</p></div>;
+}
+
+function Rules({ rule }) {
+  return (
+    <React.Fragment>
+      <TopLevel fail={rule === 'top-level'} />
+    </React.Fragment>
+  );
+}
+
+export default function RulesDemo() {
+  return <Rules rule="top-level" />;
 }
