8e0be89c2449a345e86bd5652f9351597ff936b5
codinronan <patrick@engineerapart.com>
2019-02-21 16:59:54 -0600

    Add a note to app.js re: starting point

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// START HERE:
+// Why hooks? What were Hooks specifically intended to attack?
+// https://twitter.com/acdlite/status/1024852895814930432
+
 import { getRoute } from './routes';
 
 import Null from './utils/dummy';
